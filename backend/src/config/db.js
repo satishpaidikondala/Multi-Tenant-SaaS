bfb898b667972f442645c70230159cf483e40f7f
Satish Paidikondala <113538629+satishpadikondala@users.noreply.github.com>
2025-12-26 13:33:33 +0530

    configuration of databases

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,18 +1,12 @@
-// backend/src/config/db.js
 const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
   user: process.env.DB_USER,
-  host: process.env.DB_HOST, // This will be 'database' inside Docker, 'localhost' if running locally
+  host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
-});
-
-// Test the connection
-pool.on("connect", () => {
-  console.log("Connected to the PostgreSQL database successfully.");
 });
 
 pool.on("error", (err) => {
